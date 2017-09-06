b765f3cc16c1421680cb3e879d1d0e998fa02e51
dpcairns <dpcairns@gmail.com>
2017-09-05 22:29:49 -0700

    blob size

--- src/sprites/Octopus/InkMissile.js
+++ src/sprites/Octopus/InkMissile.js
@@ -3,8 +3,8 @@ import RootSprite from '../RootSprite'
 export default class extends RootSprite {
   constructor ({ game, world, x, y, octoAngle, octoSpeed }) {
     super({ game, world, x, y, asset: 'blob' })
-    this.width = 30
-    this.height = 30
+    this.width = Math.random() * 30 + 10
+    this.height = Math.random() * 30 + 10
     this.octoAngle = octoAngle
     this.octoSpeed = octoSpeed
     this.body.collideWorldBounds = false
